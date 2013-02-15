267955745ea7160e5a5eb1d5ef0c24498c1431a9
mike <github@shoveler.de>
2013-02-15 12:18:39 +0100

    start new db class

--- src/core/Server.java
+++ src/core/Server.java
@@ -15,6 +15,12 @@ import java.util.Properties;
  */
 public class Server {
 
+    private String dbhost;
+    private String database;
+    private String dbuser;
+    private String dbpassword;
+    
+    
    
     public void configure(String host, String dbname, String dbuser, String password){
 
@@ -40,13 +46,26 @@ public class Server {
     public static void main(String[] args)  {
        try{
        Server confInst = new Server();
-       confInst.configure("localhost", "KassenSys", "root", "root");
+       //confInst.configure("localhost", "KassenSys", "root", "root");
        }catch(Exception e) {
        e.printStackTrace();
        
        }
     }
 
+    
+    
+    
+    private void loadConfig(){
+        Properties props = new Properties();
+        
+        dbhost = props.getProperty("dbhost");
+        database = props.getProperty("database");
+        dbuser = props.getProperty("dbuser");
+        dbpassword = props.getProperty("dbpassword");
+        
+    }
+    
 //    private ServerSocket server1= new ServerSocket(1234);
 //    private Socket client;
 //    private InputStream input;
